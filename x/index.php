ed1017e91eca12b6dfbc18cd2fcb94258fc42578
akshitajajoo <45394756+akshitajajoo@users.noreply.github.com>
2021-11-27 00:47:17 +0530

    Add files via upload

---
+++ x/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="user_page.css">
+</head>
+<body>
+
+<div class="sidebar">
+  <a class="active" href="home.html">Home</a>
+  <a href="transactions.html">Transactions</a>
+  <a href="transfer.html">Transfer</a>
+ 
+</div>
+
+<div class="content">
+ 
+</div>
+
+</body>
+</html>
